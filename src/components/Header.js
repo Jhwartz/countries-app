6553a540c4bd948a0be95267bcf89ef492d90a6b
Jhwartz <jhwartz@yahoo.co.uk>
2022-08-23 17:00:23 +0000

    A build-on to routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function DetailScreen() {
+function Header() {
   return (
-    <div>DetailScreen</div>
+    <nav>
+      <div>
+        <h>LOGO</h>
+      </div>
+      <ul>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/contact"}>Contact</Link></li>
+        <li><Link to={"/about"}>About</Link></li>
+      </ul>
+    </nav>
   )
 }
 
-export default DetailScreen
+export default Header
